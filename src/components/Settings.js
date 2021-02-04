5d3e766e5ca6ddb6cafa750c0865f74389c7bf24
Shashank Mahajan <shashankyahoo7@gmail.com>
2021-02-05 01:50:05 +0530

    Added Settings Page

---
+++ src/components/Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+    </div>
+  );
+};
+
+export default Settings;
